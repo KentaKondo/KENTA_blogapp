52fa0b15e5e0e417931ce29120c2bf4d3dcb6293
KEN <kenkenkonkon1207@gmail.com>
2020-11-03 20:00:09 +0900

    記事の保存を作成したよ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,32 @@
 class ArticlesController < ApplicationController
-  def index
+   def index
     @articles = Article.all
-  end
+   end
   
-  def show
+   def show
     @article = Article.find(params[:id])
-  end
-  
-  def new
+   end
+
+   def new
     @article = Article.new
    end
-  end 
+
+
+   def create
+     @article = Article.new(article_params)
+     if @article.save
+     redirect_to article_path(@article), notice: '保存できたよ！'
+     else
+      flash.now[:error] = "保存に失敗したよ"
+      render :new
+     end
+   end
+
+
+   
+   private
+   def article_params
+    params.require(:article).permit(:title, :content)
+   end
+
+end 
